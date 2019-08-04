b631e7722810c0e83bc887977651ca9733a0b98a
cmpark0126 <cmpark0126@gmail.com>
2019-08-04 18:17:33 +0900

    Add draft of sgd

--- src/optimizers/sgd.rs
+++ src/optimizers/sgd.rs
@@ -28,5 +28,17 @@ where
 {
     fn step(&mut self) {
         println!("SGD step!");
+
+        let lr : &T = &T::from_f32(-1.0 * self.learning_rate).unwrap();
+        let alpha : &T = &T::from_f32(1.0).unwrap();
+
+        for p in &self.parameters{
+            let p = &p;
+            let mut p_t = p.borrow_mut();
+            let p_g = p_t.gradient.as_ref().unwrap().borrow();
+
+            // this case we can calculate sgd in here, I will change this ambiguousity
+
+        }
     }
 }
